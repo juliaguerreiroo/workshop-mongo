9e07b6f153fb6d5aca798ed90f518432b17bd2ee
juliaguerreiroo <judalanorag@gmail.com>
2026-05-06 17:27:03 -0300

    primeiro commit

---
+++ src/main/java/com/julia/worshopmongo/WorshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.julia.worshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorshopMongoApplication.class, args);
+    }
+
+}
